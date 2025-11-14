f619f9a4212e24249959760950706f4a151f4023
Simon Kurono <simonkurono@gmail.com>
2025-11-14 03:27:25 -0800

    changed instructions

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -9,7 +9,7 @@ research_agent = Agent(
     name="ResearchAgent",
     model="gemini-2.5-flash-lite",
     instruction="""You are a specialized research agent. Your only job is to use the
-    google_search tool to find 2-3 pieces of relevant information on the given topic and present the findings with citations.""",
+    google_search tool to find 2-3 pieces of relevant information on the given topic and present the findings with citations. Find the best research possible""",
     tools=[google_search],
     output_key="research_findings", # The result of this agent will be stored in the session state with this key.
 )
